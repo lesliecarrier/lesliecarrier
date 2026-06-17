8e877d02f57efd5a35f76c9efe91f6e7d48a3287
Matt Carrier <icecreammatt@gmail.com>
2026-06-16 23:30:01 -0700

    build: skip if commands missing

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-source ./config.sh
+. ./config.sh
 git diff-index --quiet HEAD
 
-if [ "$hugo_gallery_domain" == "" ]; then
+if [ "$hugo_gallery_domain" = "" ]; then
     echo "Missing configuration values"
     exit 1
 fi
@@ -10,6 +10,14 @@ fi
 echo "Uploading to Domain $hugo_gallery_domain"
 
 echo "IMAGES"
-s3cmd sync public/ s3://$hugo_gallery_domain/
+if command -v s3cmd >/dev/null; then
+    s3cmd sync public/ s3://$hugo_gallery_domain/
+else
+    echo "Warning: s3cmd not found, skipping image sync."
+fi
 
-aws cloudfront create-invalidation --distribution-id E4OOAMFRCHT09 --paths "/*"
+if command -v aws >/dev/null; then
+    aws cloudfront create-invalidation --distribution-id E4OOAMFRCHT09 --paths "/*"
+else
+    echo "Warning: aws CLI not found, skipping CloudFront invalidation."
+fi
